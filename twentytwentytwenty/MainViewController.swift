75c24820ac2a5e5a723d3142c706e8373cfef981
bud manstrong <nbpeth@gmail.com>
2016-10-01 08:30:18 -0500

    timers pulled out of view controller

--- twentytwentytwenty/MainViewController.swift
+++ twentytwentytwenty/MainViewController.swift
@@ -1,107 +1,31 @@
 
 import UIKit
+protocol LabelDelegate:class{
+    func updateLabel(time:String);
+    func updateProgressBar(value:Float, duration:Float);
+    func resetProgressBar();
+}
 
-class MainViewController: UIViewController {
-    var restTimer:Timer?
-    var sessionTimer:Timer?
-    var eyeRestDuration:Int = 10
-    var sessionDuration:Int = 20
+class MainViewController: UIViewController, LabelDelegate {
+    let timeMachine = TimeMachine.sharedInstance
     
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var displayLabel: UILabel!
     
     @IBAction func startWasPressed(sender: AnyObject) {
-        createSessionTimer()
-    }
-    
-    func updateSessionTimer(){
-        
-        if let timer = sessionTimer {
-            let duration = timer.duration
-            if(duration < 0) {
-                flipTimers(true)
-            }
-            else{
-                timer.update()
-                displayLabel.text = String(duration)
-                updateProgressBar(Float(duration), duration: Float(sessionDuration))
-            }
-        }
-    }
-    
-    func updateRestTimer(){
-        if let timer = restTimer {
-            let duration = timer.duration
-            if(duration < 0) {
-                flipTimers(false)
-            }
-            else{
-                restTimer?.update()
-                displayLabel.text = String(duration)
-                updateProgressBar(Float(duration), duration: Float(eyeRestDuration))
-            }
-        }
+        timeMachine.createSessionTimer()
     }
     
     @IBAction func stopWasPressed(sender: AnyObject) {
-        restTimer?.stop()
-        sessionTimer?.stop()
+        timeMachine.killTimers()
         resetProgressBar()
         
     }
-    
-
-    func createSessionTimer(){
-        resetProgressBar()
-        sessionTimer = Timer(duration: sessionDuration, target: self, selector: #selector(MainViewController.updateSessionTimer))
- 
-    }
-    
-    func createRestTimer(){
-        resetProgressBar()
-        restTimer = Timer(duration: eyeRestDuration, target: self, selector: #selector(MainViewController.updateRestTimer))
-
-    }
-    
-    func flipTimers(resting:Bool){
-        if resting == true{
-            rest()
-            
-        }
-        else{
-            wake()
-        }
-    }
-    
-    func rest(){
-        if let stimer = sessionTimer {
-            stimer.stop()
-        }
-        
-        NotificationService().notifyRest()
-        createRestTimer()
-            
-    }
-    
-    func wake(){
-        if let rtimer = restTimer {
-            rtimer.stop()
-        }
-        
-        NotificationService().notifyWake()
-        createSessionTimer()
-    
-    }
-
-    func updateProgressBar(value:Float, duration:Float){
-        let progress = value < 1 ? 0.0 : (value / duration)
-
-        progressView.setProgress(progress, animated: true)
-    }
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         resetProgressBar()
+        timeMachine.labelDelegate = self
         
     }
     
@@ -109,12 +33,14 @@ class MainViewController: UIViewController {
         progressView.setProgress(0, animated: false)
     }
     
-    //    func createTimer(duration:Int){
-    //        resetProgressBar()
-    //        sessionTimer = Timer(duration: duration, target: self, selector:Selector(updateTimer("")))
-    //    }
-    //    func updateTimer(name:String){
-    //
-    //    }
-    //
+    func updateLabel(time:String){
+        displayLabel.text = time
+    }
+    
+    func updateProgressBar(value:Float, duration:Float){
+        let progress = value < 1 ? 0.0 : (value / duration)
+        progressView.setProgress(progress, animated: true)
+    }
+    
+
 }
--- twentytwentytwenty/TimeMachine.swift
+++ twentytwentytwenty/TimeMachine.swift
@@ -1,13 +1,102 @@
 
 import Foundation
+import UIKit
 
 class TimeMachine {
     var restTimer:Timer?
     var sessionTimer:Timer?
     var eyeRestDuration:Int = 10
     var sessionDuration:Int = 20
+    var labelDelegate:LabelDelegate?
     
     static let sharedInstance = TimeMachine()
+    private init() {}
     
+    func updateLabel(){
+        
+    }
     
+    @objc func updateSessionTimer(){
+        
+        if let timer = sessionTimer {
+            let duration = timer.duration
+
+            if(duration <= 0) {
+                flipTimers(true)
+            }
+            else{
+                timer.update()
+                labelDelegate?.updateLabel(String(timer.duration))
+                labelDelegate?.updateProgressBar(Float(duration), duration: Float(sessionDuration))
+            }
+        }
+    }
+    
+    @objc func updateRestTimer(){
+        if let timer = restTimer {
+            let duration = timer.duration
+            
+            if(duration <= 0) {
+                flipTimers(false)
+            }
+            else{
+                restTimer?.update()
+                labelDelegate?.updateLabel(String(timer.duration))
+                labelDelegate?.updateProgressBar(Float(duration), duration: Float(eyeRestDuration))
+            }
+        }
+    }
+    
+    func flipTimers(resting:Bool){
+        if resting == true{
+            rest()
+            
+        }
+        else{
+            wake()
+        }
+    }
+    
+    func rest(){
+        if let stimer = sessionTimer {
+            stimer.stop()
+        }
+        
+        NotificationService().notifyRest()
+        createRestTimer()
+        
+    }
+    
+    func wake(){
+        if let rtimer = restTimer {
+            rtimer.stop()
+        }
+        
+        NotificationService().notifyWake()
+        createSessionTimer()
+        
+    }
+    
+    func createSessionTimer(){
+        labelDelegate?.resetProgressBar()
+        sessionTimer = Timer(duration: sessionDuration, target: self, selector:#selector(updateSessionTimer))
+        
+    }
+
+    func createRestTimer(){
+        labelDelegate?.resetProgressBar()
+        restTimer = Timer(duration: eyeRestDuration, target: self, selector:#selector(updateRestTimer))
+        
+    }
+    
+    func updateLabels(timer:Timer){
+        labelDelegate?.updateLabel(String(timer.duration))
+        
+    }
+    
+    func killTimers(){
+        restTimer?.stop()
+        sessionTimer?.stop()
+    }
+
 }
